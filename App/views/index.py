36b09aa9a53872ea60f03b39b091ca0be75b70b1
BLY117 <97921627+BLY117@users.noreply.github.com>
2023-10-10 18:50:57 -0400

    Final Fix

--- App/views/index.py
+++ App/views/index.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify
 from App.models import db
-from App.controllers import create_user, create_admin
+from App.controllers import create_user, create_admin, get_user
 
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
 

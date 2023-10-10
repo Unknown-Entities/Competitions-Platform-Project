848401fd3214f29a14c78a279e5284676b8c3d32
Aadil-speck <60389946+Aadil-speck@users.noreply.github.com>
2023-10-10 18:34:23 +0000

    testV3

--- App/controllers/notification.py
+++ App/controllers/notification.py
@@ -1,12 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from App.models import Notification
 from flask_socketio import SocketIO
 from App.database import db
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://mydb_r94x_user:gTa2ZEbsAGd0abVR78MMhh2gRYdYfQiT@dpg-ckipcqgmccbs73bhhu90-a/mydb_r94x'
 socketio = SocketIO(app)
-#db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
 
 @socketio.on('connect')

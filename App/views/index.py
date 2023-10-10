43651604b1fba13860f95b634d827dfad8fd5f2e
BLY117 <97921627+BLY117@users.noreply.github.com>
2023-10-10 19:10:17 -0400

    Final Fix (3)

--- App/views/index.py
+++ App/views/index.py
@@ -1,29 +1,21 @@
 from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify
 from App.models import db
-from App.controllers import create_user, create_admin, get_user
+from App.controllers import create_user, create_admin
 
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
 
-initialize_check = False
-
 @index_views.route('/', methods=['GET'])
 def index_page():
-    if initialize_check:
-        print('database already initialized')
-    else:
-        db.drop_all()
-        db.create_all()
-        create_user('bob', 'bobpass', 'bob@email')
-        create_admin('rob', 'robpass', 'rob@email')
-        initialize_check = True
-        print('database intialized')
     return render_template('index.html')
 
 @index_views.route('/init', methods=['GET'])
 def init():
     db.drop_all()
     db.create_all()
-    create_user('bob', 'bobpass', 'bob101@gmail.com')
+    create_user('bob', 'bobpass', 'bob@email')
+    create_admin('rob', 'robpass', 'rob@email')
+    initialize_check = True
+    print('database intialized')
     return jsonify(message='db initialized!')
 
 @index_views.route('/health', methods=['GET'])

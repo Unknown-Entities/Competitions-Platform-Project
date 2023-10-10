452db186163c9e1e972babe9cf7cd1fcf75e80cd
Aadil-speck <60389946+Aadil-speck@users.noreply.github.com>
2023-10-10 18:07:33 +0000

    test

--- App/views/notification.py
+++ App/views/notification.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for
 from flask_jwt_extended import jwt_required, current_user as jwt_current_user
 from flask_login import current_user, login_required
+from flask_socketio import SocketIO
 
 from .index import index_views
 

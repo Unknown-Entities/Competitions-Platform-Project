be7efff43450e481b28c1aa9e05ec241369f4bb1
BLY117 <97921627+BLY117@users.noreply.github.com>
2023-10-10 16:46:38 -0400

    Revert database.py

--- App/database.py
+++ App/database.py
@@ -1,8 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from wsgi import app
 
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 def get_migrate(app):
     return Migrate(app, db)
